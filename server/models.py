2eae079b0b761b0c463713244ff426965c129802
Njokimary <marynjoki738@gmail.com>
2023-10-21 11:05:36 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -12,9 +12,23 @@ class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50),nullable=False)
+    created_at = db.Column(db.DateTime,nullable= False)
+    updated_at = db.Column(db.DateTime,nullable= False)
+    baked_goods =db.relationship('BakedGood',backref='bakeries')
+
+def __repr__(self):
+    return f'<Bakery "{self.name}">'
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Integer,nullable=False)
+    bakery_id = db.Column (db.Integer,db.ForeignKey('bakeries.id'))
+    created_at = db.Column(db.DateTime,nullable=False)
+    updated_at = db.Column(db.DateTime,nullable=False)
+
+def __repr__(self):
+    return f'<BakedGood "{self.name}">'
